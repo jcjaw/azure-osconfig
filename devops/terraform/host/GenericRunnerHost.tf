8fa14d0f9481e129b2771e9d5104c3db78fecebf
Ahmed Messaoud <ahbenmes@microsoft.com>
2022-05-13 13:15:49 -0700

    WIP

--- devops/terraform/host/GenericRunnerHost.tf
+++ devops/terraform/host/GenericRunnerHost.tf
@@ -149,7 +149,7 @@ resource "azurerm_linux_virtual_machine" "osconfigvm" {
     sku       = var.image_sku
     version   = var.image_version
   }
-  source_image_id = var.image_name != null ? data.azurerm_shared_image.customimage.id : null
+  source_image_id = data.azurerm_shared_image.customimage.count > 0 ? data.azurerm_shared_image.customimage.id : null
 
   computer_name                   = "myvm-${var.vm_name}"
   admin_username                  = "azureuser"
